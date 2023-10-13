1b8995aac9bdaa16c934a884519a2eca840adc32
Catherine Nderitu <nderituc@mail.gvsu.edu>
2023-10-13 01:46:31 -0400

    Update Hello.py
    Added links

--- Hello.py
+++ Hello.py
@@ -30,6 +30,17 @@ query_suggestions = [
     "How can I support African immigrant communities"
 ]
 
+# Predefined Links
+Sources = [
+    {"title":"Sub-Saharan African Immigrants in the United States","url":"https://www.migrationpolicy.org/article/sub-saharan-african-immigrants-united-states-2019"},
+    {"title": "Key findings about Black immigrants in the U.S.", "url": "https://www.pewresearch.org/short-reads/2022/01/27/key-findings-about-black-immigrants-in-the-u-s/"},
+    {"title":"African immigration to the United States","url":"https://en.wikipedia.org/wiki/African_immigration_to_the_United_States"},
+    {"title":"African Immigration and Relocation in U.S. History","url":"https://www.loc.gov/classroom-materials/immigration/african/"},
+    {"title":"Black Immigrants in the United States: Status, Challenges, and Impacts","url":"https://www.boundless.com/research/black-immigrants-in-the-united-states-status-challenges-and-impacts/"},
+    {"title":"African Immigrants in America: A Demographic Overview","url":"https://www.americanimmigrationcouncil.org/sites/default/files/research/african_immigrants_in_america_a_demographic_overview.pdf"}
+    # Add more predefined links here
+]
+
 # Set up the Streamlit app
 def main():
     st.title('African Collaborative Network (ACN)')
@@ -39,12 +50,9 @@ def main():
     if 'typed_query_history' not in st.session_state:
         st.session_state.typed_query_history = []
 
-    # Display query suggestions
     for i, suggestion in enumerate(query_suggestions):
         if st.button(suggestion, key=f"suggestion_button_{i}"):
-            # If a suggestion is clicked, populate the user query with the selected suggestion
             user_query = suggestion
-            # Proceed to get the response for the selected suggestion
             response_obj = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -52,12 +60,10 @@ def main():
                 ]
             )
             response = response_obj['choices'][0]['message']['content']
-
-            # Apply green color to the response text
             st.write(f'<span style="color: green;">Response for \'{user_query}\':</span>', unsafe_allow_html=True)
             st.write(response)
+            display_links(Sources)
 
-    # Handle user queries
     user_query = st.text_input('Ask anything about African Immigrants in the US:', '')
 
     if user_query:
@@ -68,20 +74,16 @@ def main():
             ]
         )
         response = response_obj['choices'][0]['message']['content']
-
-        # Apply green color to the response text
         st.write(f'<span style="color: green;">Response for \'{user_query}\':</span>', unsafe_allow_html=True)
         st.write(response)
-
-        # Append the query and response to the query history
+        display_links(Sources)
         st.session_state.typed_query_history.append({"query": user_query, "response": response})
 
-    # Display query history in the sidebar
     st.sidebar.title('Query History')
     clear_typed_query_history = st.sidebar.button("Clear Query History")
 
     if clear_typed_query_history:
-        st.session_state.typed_query_history = []  # Clear the query history
+        st.session_state.typed_query_history = []
 
     for i, entry in enumerate(st.session_state.typed_query_history):
         query = entry["query"]
@@ -89,6 +91,14 @@ def main():
         if st.sidebar.button(f"{i + 1}. {query}", key=f"typed_query_history_button_{i}"):
             st.write(f'<span style="color: green;">Response for \'{query}\':</span>', unsafe_allow_html=True)
             st.write(response)
+            display_links(Sources)
+            
+def display_links(links):
+    st.write("Sources:")
+    for link in links:
+        st.write(f"[{link['title']}]({link['url']})")
+
+
 
 if __name__ == "__main__":
     main()
